3059313a9e2a4495cc1d85138299f5fe3a7e020d
Ojiemeke-Michael <ojiemekemichaelolusegun@gmail.com>
2022-09-08 19:12:47 -0700

    made some changes to file 4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,22 @@
 #include <stdio.h>
-int main()
-{
-	char str[]="Programming is like building a multilingual puzzle.\n";
-	puts(str);
 
-	return 0;
+
+/**
+
+ * main - Entry point
+
+ *
+
+ * Return: Always 0 (Success)
+
+ */
+
+int main(void)
+
+{
+	char str[] = "\"Programming is like building a multilingual puzzle\n";
+	puts(str);
+	
+	return(0);
 }
 
