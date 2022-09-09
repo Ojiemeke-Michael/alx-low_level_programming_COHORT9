a38d253a673466b1d1b929c3f95f735c39b61346
Ojiemeke-Michael <ojiemekemichaelolusegun@gmail.com>
2022-09-08 19:27:22 -0700

    script for file 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	/*the command printf prints strings inputed into it*/
+	printf ("with proper grammar, but the outcome is a piece of art,");
+
+    	return (0);
+
+}
