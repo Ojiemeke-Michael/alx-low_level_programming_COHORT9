077fb86fffb194f5cfc903c9e19e7e81e718a7ff
Ojiemeke-Michael <ojiemekemichaelolusegun@gmail.com>
2022-09-08 21:23:05 -0700

    made some corrections to file 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,12 +5,9 @@
  * Return: Always 0 (Success)
  *
  */
-
 int main(void)
 {
 	/*the command printf prints strings inputed into it*/
 	printf ("with proper grammar, but the outcome is a piece of art,");
-
-    	return (0);
-
+	return (0);
 }
