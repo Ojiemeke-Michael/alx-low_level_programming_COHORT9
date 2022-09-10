6fad22df8ebd8e06b2c78e0672c9cc4d41f331c7
Ojiemeke-Michael <ojiemekemichaelolusegun@gmail.com>
2022-09-10 05:12:03 -0700

    made some changes to file 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-/**main - Entry point
- *
- * Return: Always 0 (Success)
- *
+/**
+ * main - Entry point
+ *Description - to print strings of words to the standard output
+ *Return: Always 0 (Success)
  */
 int main(void)
 {
 	/*the command printf prints strings inputed into it*/
-	printf ("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome
+		is a piece of art,\n");
 	return (0);
-}
