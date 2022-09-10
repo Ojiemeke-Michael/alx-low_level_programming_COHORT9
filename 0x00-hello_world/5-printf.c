e402d71da38d3fb41b869825e616ba3d610dd44e
Ojiemeke-Michael <ojiemekemichaelolusegun@gmail.com>
2022-09-10 05:20:34 -0700

    made some changes to file 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -8,7 +8,7 @@
 int main(void)
 {
 	/*the command printf prints strings inputed into it*/
-	printf("with proper grammar, but the outcome");
+	printf("with proper grammar, but the outcome ");
 	printf("is a piece of art,\n");
 	return (0);
 }
