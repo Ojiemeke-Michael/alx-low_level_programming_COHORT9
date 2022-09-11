9bf6217cbd48900efd1a4e39aa6e3f234e814306
Ojiemeke-Michael <ojiemekemichaelolusegun@gmail.com>
2022-09-11 06:05:50 -0700

    file 1-last_digit.c to assign a random value to num n and check for the value of the last digit of n

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,25 +4,31 @@
 
 /**
  * main - assigns random number to n
+ *
  * Description: prints any random number n each time it is executed
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int n;
 	int e;
-	e = n % 10;
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	if (e > 5) {
-	    printf("Last digit of %d is %d and is ", n, e);
-	    printf("greater than 5\n");
-	}else if (n == 0) {
- 	    printf ("Last digit of %d is %d and is 0\n", n, e);
-	}else if ((n < 6) && (n != 0)) {
-	    printf("Last string of %d is %d and is ", n, e);
-	    printf("less than and is not 0\n");
+	e = n % 10;
+
+	if (e > 5)
+	{
+		printf("Last digit of %d is %d and is ", n, e);
+		printf("greater than 5\n");
+	} else if (e == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, e);
+	} else if ((e < 6) && (e != 0))
+	{
+		printf("Last string of %d is %d and is ", n, e);
+		printf("less than and is not 0\n");
 	}
 	return (0);
 }
