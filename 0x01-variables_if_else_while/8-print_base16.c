35ae83c8f90e0df41501461ce279e8de2595665a
Ojiemeke-Michael <ojiemekemichaelolusegun@gmail.com>
2022-09-11 08:47:55 -0700

    file 8-print_base16.c to print all hexadecimal numbers using putchar() function in form :0123456789abcdef

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,13 +8,13 @@
  * Return: 0
  */
 int main(void)
-}
+{
 	int hd;
 	int id;
 
 	for (hd = 48; hd <= 57; hd++)
 		putchar(hd);
-	for (i = 97; i <= 102; i++)
+	for (id = 97; id <= 102; id++)
 		putchar(id);
 	putchar('\n');
 	return (0);
