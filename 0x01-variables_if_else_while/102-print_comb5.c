74d4913cab339b4c6c0ad011ac32ec28e55cdf64
Ojiemeke-Michael <ojiemekemichaelolusegun@gmail.com>
2022-09-12 18:30:33 -0700

    file 102-print_comb5.c to print combinations of numbers using putchar

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -15,7 +15,7 @@ int main(void)
 
 	for (p = 0; p <= 98; p++)
 	{
-	for (q = p + 1; p <= 99; q++)
+		for (q = p + 1; p <= 99; q++)
 		{
 			putchar((p / 10) + '0');
 			putchar((p % 10) + '0');
@@ -28,4 +28,5 @@ int main(void)
 			putchar(' ');
 		}
 	}
+	return (0);
 }
