e58b9bb60507eb592f885f64053a5a27447dfcc1
Ojiemeke-Michael <ojiemekemichaelolusegun@gmail.com>
2022-09-14 14:22:17 -0700

    made some corrections to file 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,14 +9,22 @@ void times_table(void)
 {
 	int i;
 	int n;
+	int mult;
 
 	for (i = 0; i <= 9; i++)
 	{
+		_putchar('0');
+
 		for (n = 1; n <= 9; n++)
 		{
-			_putchar(i * n);
 			_putchar(',');
 			_putchar(' ');
+
+			mult = i * n;
+			if (mult <= 9)
+				_putchar(' ');
+			else
+				_putchar((mult) / 10);
 		}
 		_putchar('\n');
 	}
