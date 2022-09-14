711fef0f1ea504f046c67f3f81f5f5ce2b45f27e
Ojiemeke-Michael <ojiemekemichaelolusegun@gmail.com>
2022-09-14 14:42:23 -0700

    file 10-add.c to add two integers together

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * add - add two integers together
+ *
+ * @num1: character to take the action.
+ *
+ * @num2: character to take the action too.
+ *
+ * Return: 0
+ */
+int add(int num1, int num2)
+{
+	int num3;
+
+	num3 = num1 + num2;
+
+	_putchar(num3);
+	return (0);
+}
