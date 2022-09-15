8385a012e0aa85b0717c7e26fb9f19f8755dfbad
Ojiemeke-Michael <ojiemekemichaelolusegun@gmail.com>
2022-09-15 08:57:53 -0700

    file 2-mul.c to multiply two integers a and b

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - function to multiply two integers.
+ *
+ * @a: first character to be multiplied
+ *
+ * @b: second charactr to be mltiplied
+ *
+ * Return: 0 Always
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
