e055cd2a59e82281d2f310e48edec83295a7357a
Ojiemeke-Michael <ojiemekemichaelolusegun@gmail.com>
2022-09-16 15:28:44 -0700

    made some corrections to file 9-fizz_buzz.c by adding new line character to end of output

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -4,7 +4,7 @@
 /**
  * main - entry point
  *
- * Description: to print nbers from 1 to 100
+ * Description: to print numbers from 1 to 100
  * with multiples of 3 as 'fizz. multiples of
  * 5 as 'Buzz' and multiples of 15 as "Fizz
  * Buzz"
@@ -17,12 +17,14 @@ int main(void)
 
 	for (num = 1; num <= 100; num++)
 	{
-		if ((num % 3 == 0) && (num % 5 == 0))
+		if ((num % 3 == 0) && (num % 5 == 0) && (num != 100))
 			printf("FizzBuzz ");
 		else if (num % 3 == 0)
 			printf("Fizz ");
 		else if (num % 5 == 0)
 			printf("Buzz ");
+		else if (num == 100)
+			printf("FizzBuzz\n");
 		else
 			printf("%d ", num);
 	}
