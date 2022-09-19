5c4ac0162adecf5704e6e84147e5857240623b61
Ojiemeke-Michael <ojiemekemichaelolusegun@gmail.com>
2022-09-19 14:45:49 -0700

    file 4-print_rev.c to print in reverse order string elements

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_rev - function to print elements of an array in reverse order.
+ *
+ * @s: array for which its characters are printed in reverse order.
+ */
+void print_rev(char *s)
+{
+	int count = 0;
+
+	while (s[count] != '\0')
+		count++;
+
+	while (count)
+		_putchar(s[count - 1]);
+
+	_putchar('\n');
+}
