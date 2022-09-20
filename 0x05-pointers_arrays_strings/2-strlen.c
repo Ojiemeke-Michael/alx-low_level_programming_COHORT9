0185d7028b09bd48e2e3ae43f5beb3036db31c81
Ojiemeke-Michael <ojiemekemichaelolusegun@gmail.com>
2022-09-20 14:18:45 -0700

    file   2-strlen.c to print the length of a string

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -9,11 +9,11 @@
  */
 int _strlen(char *s)
 {
-	int count;
+	int i, c = 0;
 
-	for (count = 0; s[count] != '\0'; count++)
+	for (i = 0; *(s + i) != '\0'; i++)
 	{
-		count = count + 1;
+		c++;
 	}
-	return (count);
+	return (c);
 }
