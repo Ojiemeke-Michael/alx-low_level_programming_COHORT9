f40b5cfe6750cf495d825cc5d0723dc8568868ba
Ojiemeke-Michael <ojiemekemichaelolusegun@gmail.com>
2022-09-23 15:54:01 -0700

    made some changes to file 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,20 +6,16 @@
  */
 void print_number(int n)
 {
-	unsigned int n1;
+	unsigned int n1 = n;
 
 	if (n < 0)
 	{
+		putchar('-');
 		n1 = -n;
-		_putchar('_');
-	}
-	else
-	{
-		n1 = n;
 	}
 
-	if (n1 / 10)
-		_putchar((n1 / 10) + '0');
+	if (n1 / 10 != 0)
+		print_number(n1 / 10);
 
 	_putchar((n1 % 10) + '0');
 }
