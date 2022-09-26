25eaae73d28242251711bec92cecb3146e21ab6f
Ojiemeke-Michael <ojiemekemichaelolusegun@gmail.com>
2022-09-26 16:06:11 -0700

    file 5-strstr.c to lkkk

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strstr - locates a substring
+ * @haystack: the compount string. the string to be seatched.
+ * @needle: the substring doing the searching
+ * Return: pointer haystack or null
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	unsigned int i = 0, j = 0;
+
+	while (haystack[i])
+	{
+		while (needle[j] && (haystack[i] == needle[0]))
+		{
+			if (haystack[i + j] == needle[j])
+				j++;
+			else
+				break;
+		}
+		if (needle[j])
+		{
+			i++;
+			j = 0;
+		}
+		else
+			return (haystack + i);
+	}
+	return (0);
+}
