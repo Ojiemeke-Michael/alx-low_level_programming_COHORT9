251b843eb384aa76bf3a1b8c411cc7da816ea690
Ojiemeke-Michael <ojiemekemichaelolusegun@gmail.com>
2022-09-28 14:32:10 -0700

    made some changes to file 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,13 +6,13 @@
  */
 void _puts_recursion(char *s)
 {
-	{
 	if (*s == '\0')
 	{
-		return ('\n');
+		_putchar('\n');
 	}
-	_putchar(*s);
-	_puts_recursion(s + 1);
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
-	_putchar('\n');
 }
