e413f00166e5dc3b7f7bbb0ffe645e458f4d7a3e
Ojiemeke-Michael <ojiemekemichaelolusegun@gmail.com>
2022-09-27 17:05:25 -0700

    included #include <stddef.h> to main.h header file and made some changes to file 4-strpbrk.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H_
 #define MAIN_H_
 
+#include <stddef.h>
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
