5df297043d331d6ea85e42eed4a1b79b0cac9c6d
Ojiemeke-Michael <ojiemekemichaelolusegun@gmail.com>
2022-09-29 16:40:51 -0700

    made a file 100-is_palindrome.c which contains function to check if a string is a palindrome or not

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,64 @@
+#include "main.h"
+int _strlength(char *s);
+void palin_check(char *s, int start, int end, int *flag);
+int is_palindrome(char *s);
+
+/**
+ * _strlength - finds the length of the string which is being
+ * checked if it is a palindrome.
+ *
+ * @s: pointer to the string.
+ *
+ * Return: unsigned integer value "len".
+ */
+int _strlength(char *s)
+{
+	int len = 1;
+
+	if (*s == '\0')
+		return (0);
+	else
+		return (len + _strlength(s + 1));
+}
+
+/**
+ * palin_check - the real function which checks the string
+ * if it is a palindrom or not.
+ *
+ * @s: string to be checked.
+ * @start: this is the starting index of the string. this is
+ * zero as C is zero index based
+ * @end: this is the end index of the string. this is gotten by
+ * _strlength(s) - 1.
+ * @flag: this contains the end value for which we will know if
+ * the string in question is a palindrome or not.
+ * Return: void i.e. no return.
+ */
+void palin_check(char *s, int start, int end, int *flag)
+{
+	const int middle = end / 2;
+
+	if (start <= middle)
+	{
+		if (*(s + start) == *(s + end))
+			*flag *= 1;
+		else
+			*flag *= 0;
+		palin_check(s, start + 1, end - 1, flag);
+	}
+}
+
+/**
+ * is_palindrome - returns the actual value of the palin_check function.
+ * this is the actual code that will be returned to the main function.
+ *
+ * @s: string to be checked if it is a palindrome or not.
+ * Return: if successful that the string is a palindrome, return 1. else 0.
+ */
+int is_palindrome(char *s)
+{
+	int flag = 1;
+
+	palin_check(s, 0, _strlength(s) - 1, &flag);
+	return (flag);
+}
