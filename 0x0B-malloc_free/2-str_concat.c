5ba0c52472bffacb80588e8b986b71b4e8892cff
Ojiemeke-Michael <ojiemekemichaelolusegun@gmail.com>
2022-10-03 16:34:16 -0700

    made some corre

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -21,8 +21,10 @@ char *str_concat(char *s1, char *s2)
 
 	len1 = 0;
 	len2 = 0;
-	if (s1 == NULL || s2 == NULL)
-		return (NULL);
+	if (s1 == NULL)
+		s1 = '\0';
+	if (s2 == NULL)
+		s2 = '\0';
 
 	for (i = 0; *(s1 + i) != '\0'; i++)
 		len1++;
