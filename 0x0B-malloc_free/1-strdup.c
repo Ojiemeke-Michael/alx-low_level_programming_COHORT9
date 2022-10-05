6d133f70bfc9be11740689bf7b6dc271873c5d4d
Ojiemeke-Michael <ojiemekemichaelolusegun@gmail.com>
2022-10-05 15:21:01 -0700

    made some changes to file 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -21,7 +21,7 @@ char *_strdup(char *str)
 
 	ptr = malloc((len + 1) * sizeof(char));
 
-	if (ptr == NULL || str == NULL)
+	if (str == NULL)
 		return (NULL);
 	for (i = 0; *(str + i) != '\0'; i++)
 	{
