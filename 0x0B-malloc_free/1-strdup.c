1f7d36f17e1ddeae2d7b7a230a81312948d3af29
Ojiemeke-Michael <ojiemekemichaelolusegun@gmail.com>
2022-10-05 15:07:43 -0700

    made a file 1-strdup.c to returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter.

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strdup - function using dynamic memory allocation that returns
+ * a pointer to a newly allocated space in memory, which
+ * contains a copy of the string given as a parameter.
+ *
+ * @str: string from which its contents are copied into pointer ptr.
+ *
+ * Return: returns  pointer ptr on success, returns NULL on failure.
+ */
+char *_strdup(char *str)
+{
+	char *ptr;
+	int len;
+	int i;
+
+	len = 0;
+	for (i = 0; *(str + i) != '\0'; i++)
+		len++;
+
+	ptr = malloc((len + 1) * sizeof(char));
+
+	if (ptr == NULL || str == NULL)
+		return (NULL);
+	for (i = 0; *(str + i) != '\0'; i++)
+	{
+		*(ptr + i) = *(str + i);
+	}
+
+	*(ptr + i) = '\0';
+	return (ptr);
+}
