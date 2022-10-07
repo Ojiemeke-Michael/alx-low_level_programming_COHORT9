8943faa07049418c7c3918fd7a7b869ebb49e11e
Ojiemeke-Michael <ojiemekemichaelolusegun@gmail.com>
2022-10-06 17:46:53 -0700

    made some corrections to file 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -48,7 +48,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	for (i = 0; *(s1 + i) != '\0'; i++)
 	{
-		*(ptr + i) = *(str + i);
+		*(ptr + i) = *(s1 + i);
 	}
 
 	for (j = 0; j < n; j++)
