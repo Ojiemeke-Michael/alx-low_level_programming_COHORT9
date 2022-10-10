232a390e4a3c07a29719a882693397d55790238f
Ojiemeke-Michael <ojiemekemichaelolusegun@gmail.com>
2022-10-10 15:35:42 -0700

    file 1-init_dog.c which initializes a variable of type 'struct dog'

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -9,7 +9,7 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = "Poppy";
-	d->age = 3.5;
-	(*d).owner = "Bob";
+	d->name = name;
+	d->age = age;
+	(*d).owner = owner;
 }
