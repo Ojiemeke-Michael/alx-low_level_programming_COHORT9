b256937d3602d612b644468aba01fecf7e4acab5
Ojiemeke-Michael <ojiemekemichaelolusegun@gmail.com>
2022-10-10 03:34:47 -0700

    files used i  learning how to use structures in C programming language

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+An ALX project that deals with what structures are, learning structures, when, why and how to use them and How to use typedef.
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H_
+#define DOG_H_
+/**
+ * struct dog - structure containing dog's information.
+ * @name: name of dog.
+ * @age: age of dog in decimal point.
+ * @owner: thw owber of the dog.
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
