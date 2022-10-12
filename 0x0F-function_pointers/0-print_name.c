27bea4b4f40b85cc464b34f72572d91faab16bc4
Ojiemeke-Michael <ojiemekemichaelolusegun@gmail.com>
2022-10-12 14:23:50 -0700

    made a test file 0-main.c to test file 0-print_name.c and fixed some bugs in 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,7 +3,8 @@
 /**
  * print_name - prints name of the input.
  * @name: the name to be printed.
- * @f: the action function i.e. the function which contains the formatting of the name.
+ * @f: the action function i.e. the function
+ * which contains the formatting of the name.
  */
 void print_name(char *name, void (*f)(char *))
 {
