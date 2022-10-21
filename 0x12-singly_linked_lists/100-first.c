f6c3aa14c1cc75ee2a3da89ae0ed68961680de2f
Ojiemeke-Michael <ojiemekemichaelolusegun@gmail.com>
2022-10-21 08:18:12 -0700

    created file 100-first.c that prints strings before the main function is executed

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * before_main - prints strings befor main is executed.
+ *
+ * Return: void.
+ */
+void __attribute__((constructor))before_main()
+{
+	printf("You are beat! and yet, you must allow\n");
+	printf("I bore my house upon my back!\n");
+}
